d854cf7bceec6aefd62531114b2163cb0a397821
554158 <554158@PC378477>
2019-04-02 20:44:09 +0530

    Add project files.

---
+++ MSTestJenkins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestJenkins
+{
+    public class Test
+    {
+        public string m1(string name)
+        {
+            return "Welcome " + name;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.m1("Marish");
+        }
+    }
+}
---
+++ MSTestJenkinsTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestJenkins;
+using System.Diagnostics;
+
+namespace MSTestJenkinsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Test t = new Test();
+            var res = t.m1("Marish");
+            Trace.WriteLine(res);
+        }
+    }
+}
