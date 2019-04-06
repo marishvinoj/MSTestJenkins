7b4e80a90ca3beb88a184448fba8fa90709ab22c
marishvinoj <marishvinojb@gmail.com>
2019-04-06 12:05:00 +0530

    commit

--- MSTestJenkinsTests/UnitTest1.cs
+++ MSTestJenkinsTests/UnitTest1.cs
@@ -8,23 +8,23 @@ namespace MSTestJenkinsTests
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //    Test t = new Test();
-        //    var res = t.m1("Marish");
-        //    Trace.WriteLine(res);
-        //    Assert.IsNotNull(t);
-        //}
-
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethod1()
         {
             Test t = new Test();
             var res = t.m1("Marish");
             Trace.WriteLine(res);
-            Assert.IsNull(t);
+            Assert.IsNotNull(t);
         }
+
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    Test t = new Test();
+        //    var res = t.m1("Marish");
+        //    Trace.WriteLine(res);
+        //    Assert.IsNull(t);
+        //}
 
     }
 }
